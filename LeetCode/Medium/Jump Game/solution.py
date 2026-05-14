5d5094ec48c1443fc9f27e014943e592d1c028fa
Reshma-Thouti <thoutireshma@gmail.com>
2026-05-14 13:39:18 +0530

    Solved Jump Game

---
+++ LeetCode/Medium/Jump Game/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def canJump(self, nums):
+        m=0
+        for i in range(len(nums)):
+            if i>m:
+                return False
+            m= max(m, i + nums[i])
+        return True 
+        
